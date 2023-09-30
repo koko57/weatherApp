ee98fc4f8da0e9900a86262b6a5cc465d05afcff
Agata Kosior <agata.kosior57@gmail.com>
2023-09-30 19:09:00 +0200

    fix: restore notification test

--- src/__tests__/screens/MainScreen.test.tsx
+++ src/__tests__/screens/MainScreen.test.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {rest} from 'msw';
 import {server} from '../../../mocks/server';
-import {render, waitFor} from '@testing-library/react-native';
+import {render, waitFor, fireEvent} from '@testing-library/react-native';
 import {MainScreen} from '../../screens/MainScreen';
 import {BASE_URL} from '../../api/constants';
 
 // Mock the NotificationModuleInterface
-// jest.mock('../../modules/notifications', () => ({
-//     showAppNotification: jest.fn(),
-// }));
+jest.mock('../../modules/notifications', () => ({
+    showAppNotification: jest.fn(),
+}));
 
 const mockedNavigate = jest.fn();
 
@@ -34,29 +34,29 @@ describe('MainScreen', () => {
         expect(getByText('Madrid')).toBeDefined();
     });
 
-    // it('triggers notification on button press', async () => {
-    //     const {getByTestId} = render(<MainScreen />);
-    //
-    //     // Wait for the data to load
-    //     await waitFor(() => getByTestId('list'));
-    //
-    //     // Mock the Math.random function to always return 0
-    //     const originalMathRandom = Math.random;
-    //     Math.random = () => 0;
-    //
-    //     // Click the notification button
-    //     fireEvent.press(getByTestId('notificationButton'));
-    //
-    //     // Restore the original Math.random function
-    //     Math.random = originalMathRandom;
-    //
-    //     // Check if the showAppNotification function was called with the expected arguments
-    //     const {showAppNotification} = require('../../modules/notifications');
-    //     expect(showAppNotification).toHaveBeenCalledWith(
-    //         'Weather in Paris',
-    //         'mist',
-    //     );
-    // });
+    it('triggers notification on button press', async () => {
+        const {getByTestId} = render(<MainScreen />);
+
+        // Wait for the data to load
+        await waitFor(() => getByTestId('list'));
+
+        // Mock the Math.random function to always return 0
+        const originalMathRandom = Math.random;
+        Math.random = () => 0;
+
+        // Click the notification button
+        fireEvent.press(getByTestId('notificationButton'));
+
+        // Restore the original Math.random function
+        Math.random = originalMathRandom;
+
+        // Check if the showAppNotification function was called with the expected arguments
+        const {showAppNotification} = require('../../modules/notifications');
+        expect(showAppNotification).toHaveBeenCalledWith(
+            'Weather in Paris',
+            'mist',
+        );
+    });
 
     it('renders loading state', async () => {
         // Mock the hook returning different data
