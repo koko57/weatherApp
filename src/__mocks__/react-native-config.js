ce69715671a7da6fd37f43edb4724cf4df8fc282
Agata Kosior <agata.kosior57@gmail.com>
2024-01-24 20:26:48 +0000

    fix: fix for tests

---
+++ src/__mocks__/react-native-config.js
@@ -0,0 +1,4 @@
+export default {
+    BASE_URL: 'https://api.openweathermap.org/data/2.5/group',
+    API_KEY: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+};
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,5 +3,3 @@ import Config from 'react-native-config';
 export const API_KEY = Config.API_KEY ?? '';
 export const BASE_URL = Config.BASE_URL ?? '';
 export const ENV = Config.ENV ?? '';
-
-console.log({API_KEY, BASE_URL, ENV});
