080cc19c80721324fee8ca0f66e0bb750a94c0c8
Agata Kosior <agata.kosior57@gmail.com>
2024-01-24 07:42:31 +0000

    fix: add test env

--- src/api/fetchWeatherData.ts
+++ src/api/fetchWeatherData.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 import {API_KEY, BASE_URL} from '../constants';
+import {TEST_DATA} from './testData';
 
 export const fetchWeatherData = async (groupIds: number[]) => {
+    if (process.env.ENV === 'test') {
+        return TEST_DATA;
+    }
     const {data} = await axios.get(BASE_URL, {
         params: {
             id: groupIds.join(','),
